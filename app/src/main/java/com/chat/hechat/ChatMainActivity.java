5b831ff77bfd0a7de714cdee945a7c8331fe9dce
DKOUT <11006520+Lim9er@users.noreply.github.com>
2019-06-13 08:54:26 +0800

    Settle Places: + Contact Activities

--- app/src/main/java/com/chat/hechat/ChatMainActivity.java
+++ app/src/main/java/com/chat/hechat/ChatMainActivity.java
@@ -36,7 +36,6 @@ public class ChatMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainchat);
-
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
